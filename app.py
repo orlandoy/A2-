026e68cdd7381d3c4a99c977564ecb9222092d90
orlandoy <168177412+orlandoy@users.noreply.github.com>
2025-05-03 16:29:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -74,3 +74,9 @@ def load_initial_data(_):
 
 if __name__ == "__main__":
     app.run_server(debug=False)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8050))
+    app.run_server(debug=False, host="0.0.0.0", port=port)
+
